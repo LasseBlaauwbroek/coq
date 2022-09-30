0b8a207bc135f8ec817a2525cdcf2feb6e297336
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-09-30 21:47:26 +0200

    Backport coq/coq#16587 | Fix undefined evars in `case q: T`
    Take the following snippet, that compiles today:
    ```
    Require Import ssreflect.
    Goal True.
    unshelve case: (S _); auto.
    exact 0.
    Qed.
    ```
    When adding an equation to this, ssreflect runs into trouble:
    ```
    Require Import ssreflect.
    Goal True.
    Fail unshelve case q: (S _); auto.
    Qed.
    ```
    This results in an unresolved evars anomaly on 8.16 and `Tried to normalize
    ill-typed term` on master since #16442 (not sure why this PR started hiding the
    missing evar issue).
    
    This issue happens because the evar that is substituted for the hole in `S _` is
    not added to the evar map by ssreflect. This PR remedies this in a rather insane
    way by just grafting the required evars on top of the evar map. The surrounding
    code also seems to do some rather crazy evar surgery so maybe this is okay.. :-\

--- plugins/ssr/ssrelim.ml
+++ plugins/ssr/ssrelim.ml
@@ -391,9 +391,16 @@ let ssrelim ?(is_case=false) deps what ?elim eqid elim_intro_tac =
           let erefl = fire_subst gl erefl in
           let erefl_ty = Retyping.get_type_of (pf_env gl) (project gl) erefl in
           let eq_ty = Retyping.get_type_of (pf_env gl) (project gl) erefl_ty in
+          let evds = Evar.Map.bind (Evd.find gl.Evd.sigma) @@
+            Evd.evars_of_term gl.Evd.sigma new_concl in
           let gen_eq_tac s =
             let open Evd in
-            let sigma = merge_universe_context s.sigma (evar_universe_context (project gl)) in
+            let sigma, shelve = Evar.Map.fold (fun e info (sigma, shelve) ->
+                if not @@ Evd.mem sigma e then
+                  Evd.add sigma e info, e::shelve else
+                  (sigma, shelve))
+                evds (s.sigma, []) in
+            let sigma = merge_universe_context sigma (evar_universe_context (project gl)) in
             apply_type new_concl [erefl] { s with sigma }
           in
           gen_eq_tac, eq_ty, gl in
