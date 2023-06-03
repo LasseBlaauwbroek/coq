02708008c58ada0b13deb7ee2f723da3d89c22ee
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-06-03 16:01:17 +0200

    Fix crash of elim H using tt. (Fixed upstream in coq/coq#17106)

--- tactics/tactics.ml
+++ tactics/tactics.ml
@@ -1404,13 +1404,13 @@ let clenv_refine_in ?err with_evars targetid id sigma0 clenv tac =
 let last_arg sigma c = match EConstr.kind sigma c with
   | App (f,cl) ->
       Array.last cl
-  | _ -> anomaly (Pp.str "last_arg.")
+  | _ -> raise (Invalid_argument "last_arg.")
 
 let nth_arg sigma i c =
   if Int.equal i (-1) then last_arg sigma c else
   match EConstr.kind sigma c with
   | App (f,cl) -> cl.(i)
-  | _ -> anomaly (Pp.str "nth_arg.")
+  | _ -> raise (Invalid_argument "nth_arg.")
 
 let index_of_ind_arg sigma t =
   let rec aux i j t = match EConstr.kind sigma t with
@@ -1475,7 +1475,7 @@ let general_elim_clause with_evars flags where indclause elim =
   let indmv =
     (match EConstr.kind sigma (nth_arg sigma i elimclause.templval.rebus) with
        | Meta mv -> mv
-       | _  -> user_err ~hdr:"elimination_clause"
+       | _ | exception Invalid_argument _ -> user_err ~hdr:"elimination_clause"
              (str "The type of elimination clause is not well-formed."))
   in
   match where with
